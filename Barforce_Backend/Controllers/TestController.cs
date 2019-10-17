d6f6aee1992e5c955a0a994df9afa91cff59a65d
Justus Hueske <justus.hueske@tobit.software>
2019-10-17 12:05:00 +0200

    :tada: Init project

---
+++ Barforce_Backend/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barforce_Backend.Controllers
+{
+    [Route("api/test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
