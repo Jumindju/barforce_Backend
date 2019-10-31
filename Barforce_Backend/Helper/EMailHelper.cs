ed76b1b362da94874669dcdc89f7f27f0ee714af
Justus Hueske <justus.hueske@tobit.software>
2019-10-31 15:17:57 +0100

    :sparkles: Add email helper

---
+++ Barforce_Backend/Helper/EMailHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Barforce_Backend.Interface.Helper;
+using Barforce_Backend.Model.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Barforce_Backend.Helper
+{
+    public class EMailHelper : IEmailHelper
+    {
+        private readonly EMailOptions _eMailOptions;
+
+        public EMailHelper(IOptions<EMailOptions> eMailOptions)
+        {
+            _eMailOptions = eMailOptions.Value;
+        }
+
+        public async Task SendVerifyMail(string receiverAddress, Guid verifyGuid)
+        {
+            var fromAddress = new MailAddress(_eMailOptions.Address);
+            var toAddress = new MailAddress(receiverAddress);
+
+            var smtpClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(_eMailOptions.Address, _eMailOptions.Password)
+            };
+
+            using var msg = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = "Anmeldung bei barfoce",
+                Body = ""
+            };
+
+            await smtpClient.SendMailAsync(msg);
+        }
+    }
+}
