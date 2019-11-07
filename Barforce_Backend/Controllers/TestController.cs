aed4e9727a066158b7f2c7cfb952adda96803ac8
Justus Hueske <justus.hueske@tobit.software>
2019-11-07 09:36:35 +0100

    :white_check_mark: Add endpoint to test auth

--- Barforce_Backend/Controllers/TestController.cs
+++ Barforce_Backend/Controllers/TestController.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Barforce_Backend.Controllers
 {
     [Route("api/test")]
     public class TestController : Controller
     {
+        [Authorize]
         [HttpGet]
         public IActionResult Test()
         {
