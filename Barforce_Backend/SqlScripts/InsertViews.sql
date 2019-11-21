8f4f2439da8b4943a7451a6b119b0b482819c608
Justus Hueske <justus.hueske@tobit.software>
2019-11-21 12:04:43 +0100

    :card_file_box: Add machineId to container view

--- Barforce_Backend/SqlScripts/InsertViews.sql
+++ Barforce_Backend/SqlScripts/InsertViews.sql
@@ -1,67 +1,58 @@
 ﻿CREATE VIEW viUser AS
-    SELECT
-        userid,
-        username,
-        email,
-        birthday,
-        weight,
-        password,
-        salt,
-        groups,
-        gender,
-        verified,
-        currenttoken
-    FROM
-         "user"
-    WHERE
-        deletetime IS NULL;
+SELECT userid,
+       username,
+       email,
+       birthday,
+       weight,
+       password,
+       salt,
+       groups,
+       gender,
+       verified,
+       currenttoken
+FROM "user"
+WHERE deletetime IS NULL;
 
 CREATE VIEW viGlassSize AS
-    SELECT
-        Id,
-        size
-    FROM
-         glasssize
-    WHERE
-        deletetime IS NULL;
+SELECT Id,
+       size
+FROM glasssize
+WHERE deletetime IS NULL;
 
 CREATE VIEW viDrink AS
-    SELECT
-        d.id,
-        g.size
-    FROM
-        drink d JOIN
-        viGlassSize g ON glasssizeid=g.Id;
+SELECT d.id,
+       g.size
+FROM drink d
+         JOIN
+     viGlassSize g ON glasssizeid = g.Id;
 
 CREATE VIEW viIngredient AS
-    SELECT
-        id,
-        name,
-        alcohollevel,
-        background
-    FROM
-        ingredient
-    WHERE
-        deletetime IS NULL;
+SELECT id,
+       name,
+       alcohollevel,
+       background
+FROM ingredient
+WHERE deletetime IS NULL;
 
 CREATE VIEW viMachine AS
-    SELECT
-        Id,
-        name
-    FROM
-        machine
-    WHERE
-        deletetime IS NULL;
+SELECT Id,
+       name
+FROM machine
+WHERE deletetime IS NULL;
 
 CREATE VIEW viContainer AS
-    SELECT
-        c.id,
-        ingredientid,
-        fillingvolume,
-        fillinglevel,
-        m.name MachineName
-    FROM
-        container c JOIN
-        viMachine m on c.machineid=m.Id
-    WHERE
-        c.deletetime IS NULL
+SELECT c.id,
+       ingredientid,
+       fillingvolume,
+       fillinglevel,
+       m.name    MachineName,
+       m.id   as machineId,
+       i.name AS ingredientName,
+       i.alcohollevel,
+       i.background
+FROM container c
+         JOIN
+     viMachine m on c.machineid = m.Id
+         JOIN
+     viingredient i on i.id = c.ingredientid
+WHERE c.deletetime IS NULL
