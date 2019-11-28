872b33763bb3dcd2c8ee37c9436f1d9ca3c968ee
Justus Hueske <justus.hueske@tobit.software>
2019-11-28 14:29:11 +0100

    :sparkles: Add method to read ordered drinks of user

--- Barforce_Backend/Interface/Repositories/IDrinkRepository.cs
+++ Barforce_Backend/Interface/Repositories/IDrinkRepository.cs
@@ -2,11 +2,13 @@
 using System.Threading.Tasks;
 using Barforce_Backend.Model.Drink;
 using Barforce_Backend.Model.Drink.Favourite;
+using Barforce_Backend.Model.Drink.Overview;
 
 namespace Barforce_Backend.Interface.Repositories
 {
     public interface IDrinkRepository
     {
+        Task<List<OverviewDrink>> ReadUsersHistory(int userId, int take, int skip);
         Task<IEnumerable<GlassSize>> ReadGlassSizes();
         Task<int> CreateOrder(int userId, int machineId, CreateDrink newDrink);
         Task<int> AddFavourite(int userId, NewFavouriteDrink newNewFavourite);
